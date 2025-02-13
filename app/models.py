89763f8d0d45411f867562f1a2eeb3186b85322b
Dmitry Kovalev <pzhabbiu@gmail.com>
2025-02-13 17:53:52 +0300

    Небольшой рефакторинг

--- app/models.py
+++ app/models.py
@@ -6,10 +6,17 @@ class User(BaseModel):
     username: str
     password: str
 
+
 class UserResponse(BaseModel):
     id: int
     username: str
 
+
 class UserRequest(BaseModel):
     username: str
     password: str
+
+
+class AccessToken(BaseModel):
+    access_token: str
+    token_type: str = 'bearer'
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from fastapi import Depends, APIRouter, HTTPException, status
 
-from models import UserResponse, UserRequest
+from models import UserResponse, UserRequest, AccessToken
 from data import UserMapper
 from services.token_services import create_access_token, get_username_from_token
 from utils import verify_password
@@ -11,20 +11,20 @@ router = APIRouter()
 
 
 @router.post('/register')
-async def register(request_user: UserRequest) -> dict:
+async def register(request_user: UserRequest) -> AccessToken:
     """Регистрация пользователей."""
 
     try:
         user_id = UserMapper.create(request_user)
     except sqlite3.IntegrityError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={'error': 'Username already exists'})
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={'error': 'Username already exists'}
+        )
     else:
         user = UserMapper.find_by_id(user_id)
 
-        return {
-            'access_token': create_access_token({'sub': user.username}),
-            'token_type': 'bearer',
-        }
+        return AccessToken(access_token=create_access_token({'sub': user.username}))
 
 
 @router.post('/auth')
@@ -33,11 +33,12 @@ async def auth(request_user: UserRequest):
 
     user = UserMapper.find_by_username(request_user.username)
     if user and verify_password(request_user.password, user.password):
-        return {
-            'access_token': create_access_token({'sub': user.username}),
-            'token_type': 'bearer',
-        }
-    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={'error': 'Invalid credentials'})
+        return AccessToken(access_token=create_access_token({'sub': user.username}))
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={'error': 'Invalid credentials'}
+        )
 
 
 @router.get('/me', response_model=UserResponse)
@@ -49,4 +50,7 @@ async def me(username: str = Depends(get_username_from_token)):
     if user:
         return user
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={'error': 'User not found'})
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={'error': 'User not found'}
+        )
